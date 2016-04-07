094c7c03e69b8a7786ab5cac9b753fea538f91b4
FlushDNS <12454339@students.lincoln.ac.uk>
2016-04-07 01:37:54 +0100

    Random Greetings; Delays AI Output;Removes punctuation from input; Adds chat intro; Adds "Tom is typing..." temp; Repeats main chat.
    10 possible random greetings.
    Function delaying AI output whilst adding "Tom is typing...". Removes
    "Tom is typing..." when output is entered.
    Output delay is based on amount of characters in output.
    Attempted to pass string to GetResponse

--- GroupProject-TOM/Program.cs
+++ GroupProject-TOM/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GroupProject_TOM
@@ -10,89 +11,109 @@ namespace GroupProject_TOM
 	{
 		
 
-		static void Main(string[] args)
-		{           
-            ResponseFinder responses = new ResponseFinder(5000); //threshold
-           
-            Welcome(); //Run Welcome Function
-            Chat(); //Main Chat Function
-            ChatEnd(); //Ends the chat
+		static void Main()
+		{                      
+            Welcome(); 
+            Chat();
+            ChatEnd();
 
 
 			Console.ReadKey();
 		}
-
-        static void Welcome() //Will only be ran once. 
+        
+        static void Welcome() //Runs once. Generates random welcome.
         {
             bool Welcome = true;
             if (Welcome == true)
             {
-                Console.WriteLine("Hi, My name is Tom, How can I assist you today?"); //TODO/Random Welcome//
+                Console.WriteLine("-You have been connected to: Tom-"); 
+                Thread.Sleep(4000);
+                Random Roll = new Random();
+                int Greeting = Roll.Next(1, 11);
+                switch (Greeting)
+                {
+                    case 1: string a = "Hi, My name is Tom. How can I help you today?"; DelayOutput(a);
+                        Console.WriteLine(a); break;
+                    case 2: string b = "Hello. I'm Tom. What seems t o be the problem?"; DelayOutput(b);
+                        Console.WriteLine(b); break;
+                    case 3: string c = "I'm Tom. What can I help you with?"; DelayOutput(c);
+                        Console.WriteLine(c); break;
+                    case 4: string d = "Hi there, this is Tom! What's up?"; DelayOutput(d);
+                        Console.WriteLine(d); break;
+                    case 5: string e = "Greetings, traveler. How can I assist?"; DelayOutput(e);
+                        Console.WriteLine(e); break;
+                    case 6: string f = "What's Up? My name is Tom, what can I help you with today?"; DelayOutput(f);
+                        Console.WriteLine(f); break;
+                    case 7: string g = "How can I help today sir? My name is Tom, "; DelayOutput(g);
+                        Console.WriteLine(g); break;
+                    case 8: string h = "I'm Tom, how can I help?"; DelayOutput(h);
+                        Console.WriteLine(h); break;
+                    case 9: string i = "You're chatting with Tom. How can I help?"; DelayOutput(i);
+                        Console.WriteLine(i); break;
+                    case 10: string j = "Tom here! Whats up?"; DelayOutput(j);
+                        Console.WriteLine(j); break;
+                }
                 Welcome = false;
             }
         }
 
-        static void Chat()
+        static void DelayOutput(string Output) //Delays outputs based on the number of chars in string
+        {                                      //Also adds "Tom is typing..." and removes it using ClearTyping()
+            int num = Output.Length;
+            num = num * 78;
+            Console.WriteLine("Tom is typing...");
+            Thread.Sleep(num);
+            ClearTyping();
+        }
+
+        static void ClearTyping() //Clears "Tom is typing" in DelayOutput()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+
+        static void Chat() //Main chat function
         {
             List<string> UserInput = new List<string>();
-            bool MainLoop = true; 
+            ResponseFinder responses = new ResponseFinder(5000); //threshold
+            bool MainLoop = true;
             if (MainLoop == true)
             {
                 string Current = "";
                 Current = Console.ReadLine();
-                FormatInput(Current); //Format the string (No spaces, punctuation)
-                UserInput.Add(Current);
+                if (Current == "")
+                {
+                    Chat();
+                }
+                else
+                {
+                    FormatInput(Current); 
+                    UserInput.Add(Current);                    
+                    responses.GetResponse(Current);
+                    return;
+                }
             }
         }
 
-        static void ChatEnd()
+        static void ChatEnd() //End function. To select chat end via emotion level
         {
             Console.WriteLine("Was that everything I can help with today?"); //TODO//Random Ending//
             string ChatEnd = Console.ReadLine();
-            bool ChatOver = false;
-            //TODO//Take ChatEnd and use ResponseFinder to find response before looping or closing.
-            FormatInput(ChatEnd);
-            if (ChatOver == true)
-            {
-                //TODO// Place random Goodbye message
-                //TODO// Terminate Application
-            }
         }
 
-        static void FormatInput(string Input)
+        static void FormatInput(string Input) //Formats user input
         {
             string UserString = Input;
-            Input = Input.Replace(" ", "");
-            Input = Input.Replace("!", "");
-            Input = Input.Replace("£", "");
-            Input = Input.Replace("$", "");
-            Input = Input.Replace("%", "");
-            Input = Input.Replace("^", "");
-            Input = Input.Replace("&", "");
-            Input = Input.Replace("*", "");
-            Input = Input.Replace("(", "");
-            Input = Input.Replace(")", "");
-            Input = Input.Replace("-", "");
-            Input = Input.Replace("_", "");
-            Input = Input.Replace("=", "");
-            Input = Input.Replace("+", "");
-            Input = Input.Replace("|", "");
-            Input = Input.Replace("<", "");
-            Input = Input.Replace(">", "");
-            Input = Input.Replace("?", "");
-            Input = Input.Replace("/", "");
-            Input = Input.Replace("[", "");
-            Input = Input.Replace("]", "");
-            Input = Input.Replace("{", "");
-            Input = Input.Replace("}", "");
-            Input = Input.Replace(";", "");
-            Input = Input.Replace(":", "");
-            Input = Input.Replace("@", "");
-            Input = Input.Replace("'", "");
-            Input = Input.Replace("#", "");
-            Input = Input.Replace("~", "");
+            string[] elements = new string[29] { " ", "!", "£", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", 
+                                               "|", "<", ">", "?", "/", "[", "]", "{", "}", ";", ":", "@", "'", "#", "~"};
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Input = Input.Replace(elements[i], "");
+            }
+
             return;
         }
-
 	}
 }
